d1d8afc7b2f018cd04119cd53ef1658df03d7826
brianly27 <brian.kyang.ly@gmail.com>
2020-04-24 18:05:46 -0700

    finished insertAtIndex method

--- linked-list.js
+++ linked-list.js
@@ -50,7 +50,7 @@ class LinkedList {
 
         // if head is null, node becomes the head
         if (this.head === null) {
-            this.head = node;
+            this.insertFirst(data);
             return
         } else {
         // if head is not null, use current to point to the head
@@ -63,14 +63,50 @@ class LinkedList {
         }
 
         // make the last node point to the node with our new data
-        // console.log("\ncurrent:", current, "\n")
         current.next = node
         this.size ++
 
     }
 
-
     // insert data at a given index
+    insertAtIndex(data, index) {
+        let current
+
+        // check to see if there is a head
+        if (index === 0) {
+            this.insertFirst(data)
+            return
+        }
+
+        // check if placing data at the desired index would also mean making it the last node in the linked list
+        if (index === this.size) {
+            this.insertLast(data)
+            return
+        }
+
+
+        //check if index is valid
+        if (index < 0 || index > this.size -1) {
+            return null
+        } else {
+            current = this.head
+            let node = new Node(data)
+            let i = 0
+            //
+            while (i < index -1) {
+                current = current.next
+                i ++
+            }
+
+            node.next = current.next
+            current.next = node
+            this.size ++
+        }
+        
+
+
+
+    }
 
     // get a node at a given index
 
@@ -79,10 +115,17 @@ class LinkedList {
     // remove all nodes
 }
 
+// console.log("\ncurrent:", current, "\n")
 let n1 = new Node(3)
 let n2 = new Node(4)
 let list1 = new LinkedList()
-list1.insertFirst(1)
-list1.insertFirst(2)
-list1.insertLast(32)
+let list2 = new LinkedList()
+list1.insertFirst("b")
+list1.insertFirst("a")
+list1.insertLast("c")
+list1.insertLast("d")
+list1.insertAtIndex("j", 0)
 list1.printData()
+
+// list2.insertLast(23)
+// list2.printData()
